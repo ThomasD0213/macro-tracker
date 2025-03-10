56cb591d269888886285a1c6e271cdeea6327035
Thomas Dalgetty <tdalgetty0213@gmail.com>
2025-03-10 17:16:21 -0600

    add more scaffolding for database access

--- lib/Services/LocalDataService.dart
+++ lib/Services/LocalDataService.dart
@@ -21,6 +21,14 @@ class User {
 
   const User({required this.id, required this.name, this.age, this.weight, this.height, this.sex, this.bmr, this.tdee});
 
+  Map<String, Object?> toMap() {
+    return {'id': id, 'name': name, 'age': age, 'weight': weight, 'height': height, 'sex': sex, 'bmr': bmr, 'tdee': tdee};
+  }
+
+  @override
+  String toString() {
+    return "User['id': $id, 'name': $name, 'age': $age, 'weight': $weight, 'height': $height, 'sex':$sex, 'bmr':$bmr, 'tdee':$tdee]";
+  }
 
 }
 
@@ -30,21 +38,25 @@ class LocalDataService {
   static bool _initialized = false; // determines whether or not initDatabase needs to be called before executing a command to the database
   LocalDataService._internal();
 
+
   initDatabase() async {
-    _database = openDatabase(
-      // Set the path to the database. Note: Using the `join` function from the
-      // `path` package is best practice to ensure the path is correctly
-      // constructed for each platform.
-      join(await getDatabasesPath(), 'database.db'),
-      onCreate: (db, version) {
-        // Run the CREATE TABLE statement on the database.
-        db.execute('CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, weight REAL, height REAL, sex TEXT, bmr REAL, tdee REAL)',);
-      },
-      // Set the version. This executes the onCreate function and provides a
-      // path to perform database upgrades and downgrades.
-      version: 1,
-    );
-    _initialized = true;
+    if(!_initialized) {
+      _database = openDatabase(
+        // Set the path to the database. Note: Using the `join` function from the
+        // `path` package is best practice to ensure the path is correctly
+        // constructed for each platform.
+        join(await getDatabasesPath(), 'database.db'),
+        onCreate: (db, version) {
+          // Run the CREATE TABLE statement on the database.
+          db.execute('CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER, weight REAL, height REAL, sex TEXT, bmr REAL, tdee REAL)',);
+        },
+        // Set the version. This executes the onCreate function and provides a
+        // path to perform database upgrades and downgrades.
+        version: 1,
+      );
+      _initialized = true;
+    }
+    print("database already initialized");
   }
 
   Future<LocalDataService> getLocalDataService() async {
@@ -55,6 +67,31 @@ class LocalDataService {
     return _localDataService;
   }
 
+  Future<dynamic> getDatabase() async {
+    if(!_initialized) {
+      await initDatabase();
+    }
+    return _database;
+  }
+
+  Future<void> insertUser(User user) async {
+    // Get a reference to the database.
+    final db = await getDatabase();
+
+    // Insert the Dog into the correct table. You might also specify the
+    // `conflictAlgorithm` to use in case the same dog is inserted twice.
+    //
+    // In this case, replace any previous data.
+    await db.insert(
+      'users',
+      user.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<void> insertFood(Food food) async {
+
+  }
   
 
 
